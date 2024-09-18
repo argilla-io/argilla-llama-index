4708a387f2263220b6202f0ec9c5c2a88bf6dede
Sara Han <127759186+sdiazlor@users.noreply.github.com>
2024-09-18 12:30:15 +0200

    Update src/argilla_llama_index/llama_index_handler.py
    Co-authored-by: David Berenstein <david.m.berenstein@gmail.com>

--- src/argilla_llama_index/llama_index_handler.py
+++ src/argilla_llama_index/llama_index_handler.py
@@ -235,7 +235,7 @@ class ArgillaSpanHandler(BaseSpanHandler[SimpleSpan], extra="allow"):
         rating_questions = [
             RatingQuestion(
                 name=f"rating_retrieved_document_{doc + 1}",
-                title=f"Rate the relevance of the Retrieved document {doc + 1}, if applicable.",
+                title=f"Rate the relevance of the Retrieved document {doc + 1}, if present.",
                 values=list(range(1, 8)),
                 description=f"Rate the relevance of the retrieved document {doc + 1}, if applicable.",
                 required=False,
